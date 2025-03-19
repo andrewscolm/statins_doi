c2db41ff732172c77bdd0393139000affcf7787a
Colm Andrews <73839417+andrewscolm@users.noreply.github.com>
2025-03-19 19:04:51 +0000

    Add codelists

---
+++ analysis/codelists.py
@@ -0,0 +1,16 @@
+####################################################################
+# This script fetches all of the codelists identified in 
+# codelists.txt from OpenCodelists.
+#
+# Author: Colm Andrews 
+#   Bennett Institute for Applied Data Science
+#   University of Oxford, 2025
+#####################################################################
+
+# --- IMPORT STATEMENTS ---
+
+## Import code building blocks from cohort extractor package
+from ehrql import codelist_from_csv
+
+ 
+# --- CODELISTS ---
